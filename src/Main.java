e25c7732070be5f824ebf5b0042ebbd25ac31e5e
Shubh Mittal <shubhctw@gmail.com>
2020-01-12 01:23:29 -0700

    Got HTML from url, cant convert to plain txt yet

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
 
+    static HTMLSource source;
+    static HTMLtoPlainText plainText;
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        source = new HTMLSource();
+        plainText = new HTMLtoPlainText();
+
+        String url = "https://en.wikipedia.org/wiki/List_of_richest_people_in_the_world";
+        String html = source.getHTMLcode(url);
+        System.out.println(html);
+        System.out.println(plainText.getPlainText(html));
     }
 }
